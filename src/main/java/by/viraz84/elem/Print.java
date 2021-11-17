e99365982dd9fc845082f3f1cb9601bc65e95d29
vlad713037 <viraz84@mail.ru>
2021-11-17 22:44:09 +0300

    2021

---
+++ src/main/java/by/viraz84/elem/Print.java
@@ -0,0 +1,16 @@
+package by.viraz84.elem;
+
+public class Print {
+    int a =3;
+    String string;
+    int b;
+
+    public Print(String string, int b) {
+        this.string = string;
+        this.b = b;
+    }
+
+
+
+
+}
---
+++ src/main/java/by/viraz84/elem/Runner.java
@@ -0,0 +1,10 @@
+package by.viraz84.elem;
+
+
+
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaa");
+    }
+}
